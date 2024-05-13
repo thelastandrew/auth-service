8f8fc0d5904d352e6cc913ed9459dad4fa6def9a
thelastandrew <thelastandrew@gmail.com>
2024-05-13 23:18:52 +0300

    feat: add models

---
+++ src/models/user-model.ts
@@ -0,0 +1,8 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+export const userModel = model('User', UserSchema);
